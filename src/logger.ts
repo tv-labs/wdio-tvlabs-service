1532f56afd1a54c64635d672c4cc796175e83490
Regan Karlewicz <regan@tvlabs.ai>
2025-03-28 17:50:14 -0700

    Update logger name

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,4 @@
 import logger from '@wdio/logger';
 import type { Logger } from '@wdio/logger';
 
-export const log: Logger = logger('wdio-tvlabs-service');
+export const log: Logger = logger('@tvlabs/wdio-service');
